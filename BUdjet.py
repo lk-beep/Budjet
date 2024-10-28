caec5db89c35598e6e7dc5b4e1baff1cabf3fdd1
lk-beep <prog10695@gmail.com>
2024-10-28 13:37:15 +0200

    tet

--- BUdjet.py
+++ BUdjet.py
@@ -1,21 +1,64 @@
-Budjet = int(input("Paljonko rahaa sinulla on budjetoida? "))
-Vuokra = int(input("Paljonko rahaa käytät vuokraan? "))
-Vakuutukset = int(input("Paljonko rahaa käytät Vakuutuksiin? "))
-Ruoka = int(input("Paljonko rahaa käytät ruokaan? "))
-Sähkö = int(input("Paljonko rahaa käytät sähköön? "))
-Liikkuminen = int(input("Paljonko rahaa käytät Liikkumiseen? "))
+import csv
+import matplotlib.pyplot as plt
+# Kysytään käyttäjältä ennakkoon määritellyt budjettitiedot
+NyVuokra = int(input("Paljonko rahaa käytät vuokraan?: "))
+NyVakuutukset = int(input("Paljonko rahaa käytät vakuutuksiin?: "))
+NyRuoka = int(input("Paljonko rahaa käytät ruokaan?: "))
+NySähkö = int(input("Paljonko rahaa käytät sähköön?: "))
+NyLiikkuminen = int(input("Paljonko rahaa käytät liikkumiseen?: "))
 
+# Alustetaan budjetti sanakirjaan
 Budjetti = {
-    "maksimi": Budjet,
-    "vuokra": Vuokra,
-    "vakuutus": Vakuutukset,
-    "ruoka": Ruoka,
-    "sähkö": Sähkö,
-    "liiku": Liikkuminen,
+    "vuokra": NyVuokra,
+    "vakuutus": NyVakuutukset,
+    "ruoka": NyRuoka,
+    "sahko": NySähkö,
+    "liiku": NyLiikkuminen,
 }
-if Budjet > Vuokra + Vakuutukset + Ruoka + Sähkö + Liikkuminen:
-    print("Budjettia ei tarvitse muuttaa")
-elif Budjet < Vuokra + Vakuutukset + Ruoka + Sähkö + Liikkuminen:
-    print("Budjetti pitää muuttaa")
-    
-print(Budjetti)
+
+
+# Lisätään käyttäjän omia budjetti kohteita
+while True:
+    uusi_kohde = input("Haluatko lisätä uuden kuluerän? (k/e): ")
+    if uusi_kohde.lower() != "k":
+        break
+    kulueran_nimi = input("Anna uuden kuluerän nimi: ")
+    kulueran_summa = int(input(f"Paljonko rahaa haluat budjetoida {kulueran_nimi}?: "))
+    Budjetti[kulueran_nimi] = kulueran_summa
+
+# käyttäjän muokkaa omia budjetti kohteita
+while True:
+    muokkaus = input("Haluatko haluatko muutta jonkin kuluerän? (k/e): ")
+    if muokkaus.lower() != "k":
+        break
+    print(Budjetti)
+    kulueran_nimi = input("Anna muutettavan kuluerän nimi: ")
+    kulueran_summa = int(input(f"Paljonko rahaa haluat budjetoida {kulueran_nimi}?: "))
+    Budjetti.update({kulueran_nimi:kulueran_summa})
+
+# tulosta budjetti csv'een
+data = Budjetti.copy()
+with open('Budjetti.csv', mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Key', 'Value'])
+    for key, value in data.items():
+        writer.writerow([key, value])
+
+# Tulosta lopullinen budjetti
+print("\nLopullinen budjetti:")
+for nimi, summa in Budjetti.items():
+    print(f"{nimi.capitalize()}: {summa} €")
+
+# Piirrä budjettikohteet pylväsdiagrammina
+kulut = list(Budjetti.keys())
+summat = list(Budjetti.values())
+
+plt.figure(figsize=(10, 6))
+plt.bar(kulut, summat, color='skyblue')
+plt.title("Budjetti eri kuluerissä")
+plt.xlabel("Kuluerä")
+plt.ylabel("Summa (€)")
+plt.xticks(rotation=45)
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.tight_layout()
+plt.show()
